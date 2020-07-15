1124f1273c6a5a6c8206af1ee1e4401a4167e9ba
Shigehiro IDANI <idani@hirotae.com>
2020-07-15 17:51:50 +0900

    add ckeditor components

---
+++ components/Ckeditor.vue
@@ -0,0 +1,41 @@
+<template>
+  <ckEditor v-model="contents" :config="editorConfig"></ckEditor>
+</template>
+
+<script>
+import CKEditor from 'ckeditor4-vue'
+export default {
+  name: 'CKEditor4',
+  components: { ckEditor: CKEditor.component },
+  props: {
+    value: {
+      type: String,
+      required: true
+    },
+    height: {
+      type: Number,
+      required: false,
+      default: 500
+    }
+  },
+  data() {
+    return {
+      editorConfig: {
+        height: this.height
+      }
+    }
+  },
+  computed: {
+    contents: {
+      get() {
+        return this.value
+      },
+      set(value) {
+        this.$emit('input', value)
+      }
+    }
+  }
+}
+</script>
+
+<style scoped></style>
